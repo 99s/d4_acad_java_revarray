e8d435b8dc2909252d02859d8278f4dafbedad16
RoY <divi9e@gmail.com>
2016-03-12 07:14:47 +0530

    Added files via upload

---
+++ RevIntArray.java
@@ -0,0 +1,11 @@
+public class RevIntArray {
+    public static void main(String[] args) {
+        int var[]=new int[3];
+        int i;
+for (i = 0; i < array.length / 2; i++) {
+  int temp = array[i];
+  array[i] = array[array.length - 1 - i];
+  array[array.length - 1 - i] = temp;
+}
+}
+}
